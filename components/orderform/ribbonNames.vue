46456e0f4175c803c8e3fa07bafe92e92394e8b9
bulldoguk <gary@myhmbiz.com>
2022-09-18 22:27:53 -0500

    Updating for better mobile view

--- components/orderform/ribbonNames.vue
+++ components/orderform/ribbonNames.vue
@@ -8,9 +8,8 @@
     <div>
       <form>
         <div class="grid md:grid-cols-12 md:gap-6">
-          <div class="col-span-6"><h2>Ribbon 1</h2></div>
-          <div class="col-span-6"><h2>Ribbon 2</h2></div>
           <div class="col-span-6">
+            <div class="w-full"><h2>Ribbon 1</h2></div>
             <div class="relative z-0 mb-6 w-full group">
               <input
                 type="text"
@@ -28,27 +27,6 @@
                 >Name</label
               >
             </div>
-          </div>
-          <div class="col-span-6">
-            <div class="relative z-0 mb-6 w-full group">
-              <input
-                type="text"
-                name="ribbonName2"
-                id="ribbonName2"
-                class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                placeholder=" "
-                required=""
-                v-model="ribbonName2"
-                @input="makeDirty()"
-              />
-              <label
-                for="ribbonName2"
-                class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-                >Name</label
-              >
-            </div>
-          </div>
-          <div class="col-span-6">
             <div class="relative z-0 mb-6 w-full group">
               <input
                 type="text"
@@ -66,27 +44,6 @@
                 >Grade</label
               >
             </div>
-          </div>
-          <div class="col-span-6">
-            <div class="relative z-0 mb-6 w-full group">
-              <input
-                type="text"
-                name="grade2"
-                id="grade2"
-                class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                placeholder=" "
-                required=""
-                v-model="grade2"
-                @input="makeDirty()"
-              />
-              <label
-                for="grade2"
-                class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-                >Grade</label
-              >
-            </div>
-          </div>
-          <div class="col-span-6">
             <div class="relative z-0 mb-6 w-full group">
               <input
                 type="text"
@@ -104,27 +61,6 @@
                 >Sport / Activity</label
               >
             </div>
-          </div>
-          <div class="col-span-6">
-            <div class="relative z-0 mb-6 w-full group">
-              <input
-                type="text"
-                name="activity2"
-                id="activity2"
-                class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                placeholder=" "
-                required=""
-                v-model="activity2"
-                @input="makeDirty()"
-              />
-              <label
-                for="activity2"
-                class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-                >Sport / Activity</label
-              >
-            </div>
-          </div>
-          <div class="col-span-6">
             <div class="relative z-0 mb-6 w-full group">
               <input
                 type="text"
@@ -144,6 +80,58 @@
             </div>
           </div>
           <div class="col-span-6">
+            <div class="w-full"><h2>Ribbon 2</h2></div>
+            <div class="relative z-0 mb-6 w-full group">
+              <input
+                type="text"
+                name="ribbonName2"
+                id="ribbonName2"
+                class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                placeholder=" "
+                required=""
+                v-model="ribbonName2"
+                @input="makeDirty()"
+              />
+              <label
+                for="ribbonName2"
+                class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                >Name</label
+              >
+            </div>
+            <div class="relative z-0 mb-6 w-full group">
+              <input
+                type="text"
+                name="grade2"
+                id="grade2"
+                class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                placeholder=" "
+                required=""
+                v-model="grade2"
+                @input="makeDirty()"
+              />
+              <label
+                for="grade2"
+                class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                >Grade</label
+              >
+            </div>
+            <div class="relative z-0 mb-6 w-full group">
+              <input
+                type="text"
+                name="activity2"
+                id="activity2"
+                class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                placeholder=" "
+                required=""
+                v-model="activity2"
+                @input="makeDirty()"
+              />
+              <label
+                for="activity2"
+                class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                >Sport / Activity</label
+              >
+            </div>
             <div class="relative z-0 mb-6 w-full group">
               <input
                 type="text"
