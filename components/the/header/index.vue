e02d00778e254fb81ae0dbaa5d2d3310cb0e0f5d
bulldoguk <gary@myhmbiz.com>
2022-10-09 13:01:18 -0500

    Added yellow button class and demo button on header

--- components/the/header/index.vue
+++ components/the/header/index.vue
@@ -9,9 +9,8 @@
           />
           <span>Mum Shoppe</span>
         </nuxt-link>
-        <a class="button button-green" @click="findMyOrder"
-          >Find My Order</a
-        >
+        <a class="button button-green" @click="findMyOrder">Find My Order</a>
+        <a class="button button-yellow" @click="helpMeChoose">Help me Choose</a>
         <nuxt-link :to="'/admin'"> Admin </nuxt-link>
         <div><TheHeaderTotal /></div>
       </div>
@@ -46,50 +45,16 @@ export default {
     toggleMobileMenu() {
       this.showMobileMenu = !this.showMobileMenu
     },
-    findMyOrder () {
+    findMyOrder() {
       this.fireModal('findMyOrder')
     },
-    oldFindMyOrder(userEmail) {
-      let uri = `orders/listByCustomer?email=${userEmail}`
-      const payload = {}
-      payload.recipient = userEmail
-      payload.subject = 'Your Mum Shoppe orders'
-      this.$api
-        .get(uri)
-        .then((resp) => {
-          const orders = resp.data
-          payload.body = buildEmail(orders)
-        })
-        .then(() => {
-          uri = '/api/sendgrid/postEmail'
-          this.$axios.post(uri, payload).then((res) => {
-            console.log('Returned', res.data)
-          })
-        })
-        .catch((e) => {
-          console.log('Failed to get orders', e)
-        })
-      /*
-      const payload = {
-        recipient: 'gary@myhmbiz.com',
-        subject: 'Find my order',
-        body: 'Here it is!',
-      }
-      const uri = '/api/sendgrid/postEmail'
-      this.$axios
-        .post(uri, payload)
-        .then((res) => {
-          console.log('Returned', res.data)
-        })
-        .catch((e) => {
-          console.log('Failed to send', e)
-        })
-        */
+    helpMeChoose() {
+      this.fireModal('helpMeChoose')
     },
   },
 }
 
-function buildEmail (orders) {
+function buildEmail(orders) {
   let body = '<p>Here are your current Mum Shoppe orders</p>'
   body += '<ul>'
   for (const order of orders) {
