d348208f5daa6bbbb62d17558150460847347081
bulldoguk <gary@myhmbiz.com>
2022-10-09 13:30:59 -0500

    Building progress bar

---
+++ components/modal/helpMeChoose.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="modal-content">
+    <div class="modal-header">
+      <h1>Help Me Choose</h1>
+      <button
+        type="button"
+        class="btn-close box-content h-4 w-4 rounded-none border-none p-1 text-black opacity-50 hover:text-black hover:no-underline hover:opacity-75 focus:opacity-100 focus:shadow-none focus:outline-none"
+        data-bs-dismiss="modal"
+        aria-label="Close"
+      ></button>
+    </div>
+    <div class="modal-body">
+      <ProgressBar />
+    </div>
+    <div class="modal-footer">
+      <a class="button button-green" @click="saveOrder">Save order</a>
+      <a class="button button-gray" @click="resetModal">Close</a>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapMutations } from 'vuex'
+import ProgressBar from './helpMeChoose/progressBar.vue'
+export default {
+  data() {
+    return {
+    }
+  },
+  methods: {
+    ...mapMutations({
+      resetModal: 'MODAL_RESET',
+      fireModal: 'MODAL_INIT',
+    }),
+    saveOrder() {
+      this.fireModal('saveMyOrder')
+    },
+  },
+  components: { ProgressBar },
+}
+</script>
---
+++ components/modal/helpMeChoose/progressBar.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div class="progressBarWrapper">
+      <div class="barDetail" :style="progressStyle">{{ progressLabel }}%</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  setup() {},
+  props: {
+    bundleCount: {
+      type: Number,
+      default: 0,
+    },
+    sectionCount: {
+      type: Number,
+      default: 0,
+    },
+    currentPosition: {
+      type: Number,
+      default: 0,
+    },
+  },
+  data() {
+    return {
+    }
+  },
+  computed: {
+    progress() {
+        try {
+            return this.currentPosition / (this.bundleCount + this.sectionCount)
+        } catch {
+            return 0
+        }
+    },
+    progressStyle() {
+      return `width: ${this.progressLabel}%`
+    },
+    progressLabel() {
+      return (this.progress * 100).toFixed(0)
+    },
+  },
+})
+</script>
