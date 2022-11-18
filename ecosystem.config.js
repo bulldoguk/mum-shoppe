c9154420f6c3339ff15aa125b4afdc5c35160f77
bulldoguk <gary@myhmbiz.com>
2022-11-17 20:01:12 -0600

    updating ecosystem file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
         name: 'mum-shoppe',
         exec_mode: 'fork',
         instances: '1', // Or a number of instances
-        script: 'npm run serve',
+        script: 'npm run build && npm run start',
         args: 'start'
       }
     ]
